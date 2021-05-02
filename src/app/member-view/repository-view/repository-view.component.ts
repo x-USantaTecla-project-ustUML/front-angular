0ac7a337e77fcca65df513982274a369bca7a5da
MRxRafi <rafadriveclase@gmail.com>
2021-05-02 23:47:19 +0200

    Added folder icon to repository #6

--- src/app/member-view/repository-view/repository-view.component.ts
+++ src/app/member-view/repository-view/repository-view.component.ts
@@ -2,49 +2,45 @@ import {FlatTreeControl} from '@angular/cdk/tree';
 import {Component} from '@angular/core';
 import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
 
-/**
- * Food data with nested structure.
- * Each node has a name and an optional list of children.
- */
-interface FoodNode {
+interface PackageNode {
   name: string;
-  children?: FoodNode[];
+  children?: PackageNode[];
 }
 
-const TREE_DATA: FoodNode[] = [
+interface FlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+/**
+ * TODO MOCK DATA, Complete with HTTPService
+ */
+const TREE_DATA: PackageNode[] = [
   {
-    name: 'Fruit',
-    children: [
-      {name: 'Apple'},
-      {name: 'Banana'},
-      {name: 'Fruit loops'},
-    ]
-  }, {
-    name: 'Vegetables',
+    name: 'Project',
     children: [
       {
-        name: 'Green',
+        name: 'Package1',
         children: [
-          {name: 'Broccoli'},
-          {name: 'Brussels sprouts'},
+          {
+            name: 'Test package',
+            children: [
+              {name: 'Large package test to see layout problems'}
+            ]
+          },
+          {name: 'Another package'},
         ]
       }, {
-        name: 'Orange',
+        name: 'Package2',
         children: [
-          {name: 'Pumpkins'},
-          {name: 'Carrots'},
+          {name: 'Package2.package1'},
+          {name: 'Package2.package2'},
         ]
       },
     ]
   },
 ];
-
-/** Flat node with expandable and level information */
-interface ExampleFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
-}
 
 @Component({
   selector: 'app-repository-view',
@@ -53,20 +49,21 @@ interface ExampleFlatNode {
 })
 export class RepositoryViewComponent {
 
-  treeControl: FlatTreeControl<ExampleFlatNode>;
-  treeFlattener: MatTreeFlattener<FoodNode, ExampleFlatNode, any>;
-  dataSource: MatTreeFlatDataSource<FoodNode, ExampleFlatNode, any>;
+  treeControl: FlatTreeControl<FlatNode>;
+  treeFlattener: MatTreeFlattener<PackageNode, FlatNode, any>;
+  dataSource: MatTreeFlatDataSource<PackageNode, FlatNode, any>;
 
   constructor() {
-    this.treeControl = new FlatTreeControl<ExampleFlatNode>(
+    this.treeControl = new FlatTreeControl<FlatNode>(
       node => node.level, node => node.expandable);
     this.treeFlattener = new MatTreeFlattener(
       this.transformer, node => node.level, node => node.expandable, node => node.children);
     this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
     this.dataSource.data = TREE_DATA;
+    this.treeControl.expandAll();
   }
 
-  private transformer = (node: FoodNode, level: number) => {
+  private transformer = (node: PackageNode, level: number) => {
     return {
       expandable: !!node.children && node.children.length > 0,
       name: node.name,
@@ -74,6 +71,6 @@ export class RepositoryViewComponent {
     };
   }
 
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+  hasChild = (_: number, node: FlatNode) => node.expandable;
 
 }
