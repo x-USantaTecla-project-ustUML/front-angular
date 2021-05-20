6ecd805590eef8f81c35becbf35ad0d7044e57f8
MRxRafi <rafadriveclase@gmail.com>
2021-05-20 09:58:26 +0200

    Fix test

--- src/app/header-view/header-view.component.spec.ts
+++ src/app/header-view/header-view.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderViewComponent } from './header-view.component';
+import {MatDialog} from '@angular/material/dialog';
+import {DemoMaterialModule} from '../material-module';
+import {AuthService} from '../shared/auth.service';
+
+class MockAuthService {
+  constructor() {
+  }
+  isAuthenticated(): void {}
+}
 
 describe('HeaderViewComponent', () => {
   let component: HeaderViewComponent;
@@ -8,7 +17,10 @@ describe('HeaderViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderViewComponent ]
+      declarations: [ HeaderViewComponent ],
+      providers: [ MatDialog,
+        { provide: AuthService, useValue: new MockAuthService() }],
+      imports: [ DemoMaterialModule ]
     })
     .compileComponents();
   });
