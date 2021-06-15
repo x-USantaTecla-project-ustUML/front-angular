91633bb7989727553a1e8ed630222fd7120bb695
MRxRafi <rafadriveclase@gmail.com>
2021-06-15 12:21:08 +0200

    Added zoom functionality to full-screen #37

--- src/app/user-view/dialogs/expanded-image/expanded-image.component.ts
+++ src/app/user-view/dialogs/expanded-image/expanded-image.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {mouseWheelZoom} from 'mouse-wheel-zoom';
 
@@ -7,18 +7,19 @@ import {mouseWheelZoom} from 'mouse-wheel-zoom';
   templateUrl: './expanded-image.component.html',
   styleUrls: ['./expanded-image.component.css']
 })
-export class ExpandedImageComponent implements OnInit {
+export class ExpandedImageComponent implements AfterViewInit {
   src: string;
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     this.src = data.src;
+  }
+
+  ngAfterViewInit(): void {
+    console.log(document.getElementById('dialog-img'));
     const wz = mouseWheelZoom({
-      element: document.querySelector('[data-wheel-zoom]'),
+      element: document.getElementById('dialog-img'),
       zoomStep: .4
     });
     wz.setSrc(this.src);
-  }
-
-  ngOnInit(): void {
   }
 
 }
