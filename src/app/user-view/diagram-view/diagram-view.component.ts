aacdb5e4d9afe831493c6ed6eebb2abffeba7315
Jaime Pérez <jaime.perez@alumnos.upm.es>
2021-06-15 14:06:49 +0200

    image error #39

--- src/app/user-view/diagram-view/diagram-view.component.ts
+++ src/app/user-view/diagram-view/diagram-view.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnChanges} from '@angular/core';
 import {encode64} from './base64-encoder';
 import pako from 'pako';
-import { mouseWheelZoom } from 'mouse-wheel-zoom';
+import {mouseWheelZoom} from 'mouse-wheel-zoom';
 
 @Component({
   selector: 'app-diagram-view',
@@ -14,12 +14,25 @@ export class DiagramViewComponent implements OnChanges {
   diagramRoute: string;
 
   ngOnChanges(): void {
-    this.diagramRoute = 'https://www.plantuml.com/plantuml/svg/~1' + encode64(pako.deflate(this.plantUML, {level: 9})); // img para png
+    this.setDiagramRoute();
     const wz = mouseWheelZoom({
       element: document.querySelector('[data-wheel-zoom]'),
       zoomStep: .4
     });
     wz.setSrc(this.diagramRoute);
+  }
+
+  onError(): void {
+    this.plantUML = 'skinparam Handwritten true\n' +
+      'skinparam DefaultTextAlignment center\n' +
+      'skinparam NoteBackgroundColor lightyellow\n' +
+      'skinparam NoteBorderColor darkgray\\n\' +\n' +
+      'note "This member is too big for PlantUML to draw a diagram." as tbd';
+    this.setDiagramRoute();
+  }
+
+  setDiagramRoute(): void {
+    this.diagramRoute = 'https://www.plantuml.com/plantuml/svg/~1' + encode64(pako.deflate(this.plantUML, {level: 9}));
   }
 
 }
