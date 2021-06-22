6df0d8efdf27bac11592b26d67b4e1638387d135
Hector Muñoz Mérida <hector.mumartin@gmail.com>
2021-06-22 16:38:19 +0200

    update #34

---
+++ src/app/documentation-view/project-package-context-view/project-package-context-view.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectPackageContextViewComponent } from './project-package-context-view.component';
+
+describe('ProjectPackageContextViewComponent', () => {
+  let component: ProjectPackageContextViewComponent;
+  let fixture: ComponentFixture<ProjectPackageContextViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProjectPackageContextViewComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectPackageContextViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/documentation-view/project-package-context-view/project-package-context-view.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-project-package-context-view',
+  templateUrl: './project-package-context-view.component.html',
+  styleUrls: ['./project-package-context-view.component.css']
+})
+export class ProjectPackageContextViewComponent {
+
+  addAccountContext = 'add:\n' +
+    '    members:\n' +
+    '       - package: package\n' +
+    '       - class: class\n' +
+    '         modifiers: public abstract\n' +
+    '       - enum: enum\n' +
+    '       - interface: interface\n' +
+    '    relations:\n' +
+    '       - inheritance: Member\n' +
+    '         role: role\n' +
+    '       - composition: Member\n' +
+    '       - aggregation: Member\n' +
+    '         role: role\n' +
+    '       - association: Member\n' +
+    '       - use: Member\n';
+  modifyAccountContext = 'modify:\n' +
+    '  members:\n' +
+    '    - class: Class\n' +
+    '      set: NewClass\n' +
+    '  relations:\n' +
+    '    - inheritance: Member\n' +
+    '      set: NewMember\n' +
+    '      role: newRole\n';
+  deleteAccountContext = 'delete:\n' +
+    '  members:\n' +
+    '    - interface: Interface\n' +
+    '  relations:\n' +
+    '    - composition: Member \n';
+  message = 'Code copied';
+  action = 'Ok';
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  openSnackBar(): void {
+    this._snackBar.open(this.message, this.action);
+  }
+
+}
