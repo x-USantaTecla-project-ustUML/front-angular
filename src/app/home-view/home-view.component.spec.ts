22bb16dddffbaff7277c487296477f6779e2901d
MRxRafi <rafadriveclase@gmail.com>
2021-06-24 13:22:49 +0200

    Added more tests #50

--- src/app/home-view/home-view.component.spec.ts
+++ src/app/home-view/home-view.component.spec.ts
@@ -1,14 +1,27 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomeViewComponent} from './home-view.component';
+import {Router} from '@angular/router';
+
+class MockRouter {
+  constructor() {
+  }
+  navigateByUrl(url: string): void {
+  }
+}
 
 describe('HomeViewComponent', () => {
-  /*let component: HomeViewComponent;
+  let component: HomeViewComponent;
   let fixture: ComponentFixture<HomeViewComponent>;
+  let mockRouter: MockRouter;
 
   beforeEach(async () => {
+    mockRouter = new MockRouter();
     await TestBed.configureTestingModule({
-      declarations: [ HomeViewComponent ]
+      declarations: [ HomeViewComponent ],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +34,12 @@ describe('HomeViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });*/
+  });
+
+  it('navigate should call router', () => {
+    spyOn(mockRouter, 'navigateByUrl');
+    component.navigate('something');
+    expect(mockRouter.navigateByUrl).toHaveBeenCalled();
+  });
+
 });
