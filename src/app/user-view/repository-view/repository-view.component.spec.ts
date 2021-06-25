f70fbe42c795f54b2d54f118b91daa5bf61ab1d6
Pablo <pabloguijarro96@gmail.com>
2021-06-25 10:32:21 +0200

    New test 2

--- src/app/user-view/repository-view/repository-view.component.spec.ts
+++ src/app/user-view/repository-view/repository-view.component.spec.ts
@@ -21,6 +21,18 @@ describe('RepositoryViewComponent', () => {
   });
 
   it('should create', () => {
+    component.activeMemberID = null;
     expect(component).toBeTruthy();
   });
+
+  it('given directoryTree and selectedNodeId when it changes then set icons and paint', () => {
+    const directoryTree = JSON.parse('[' + '{"id": "a", "name": "a", "children": [{"id": "60d58c2f9f1f522e72062dd0", ' +
+      '"name": "newProject"}]}' + ']');
+    const activeMemberID = 'a';
+    component.directoryTree = directoryTree;
+    component.activeMemberID = activeMemberID;
+    component.ngOnChanges();
+    expect(component.dataSource.data).toEqual(directoryTree);
+  });
+
 });
