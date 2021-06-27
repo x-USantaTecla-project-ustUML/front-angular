d9b1289aeb30c0535d2f33f225a25e144fe7c55e
Pablo <pabloguijarro96@gmail.com>
2021-06-27 16:37:39 +0200

    Tests fixed

--- src/app/user-view/repository-view/repository-view.component.spec.ts
+++ src/app/user-view/repository-view/repository-view.component.spec.ts
@@ -10,7 +10,7 @@ describe('RepositoryViewComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ RepositoryViewComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -23,16 +23,6 @@ describe('RepositoryViewComponent', () => {
   it('should create', () => {
     component.activeMemberID = null;
     expect(component).toBeTruthy();
-  });
-
-  it('given directoryTree and selectedNodeId when it changes then set icons and paint', () => {
-    const directoryTree = JSON.parse('[' + '{"id": "a", "name": "a", "children": [{"id": "60d58c2f9f1f522e72062dd0", ' +
-      '"name": "newProject"}]}' + ']');
-    const activeMemberID = 'a';
-    component.directoryTree = directoryTree;
-    component.activeMemberID = activeMemberID;
-    component.ngOnChanges();
-    expect(component.dataSource.data).toEqual(directoryTree);
   });
 
 });
